366f316fb482da9f02b3cd64390c6f5f6d66cbb0
huxi <1324907549@qq.com>
2021-08-20 01:05:41 +0800

    [修改]启动运行日志转移到log/running-log/目录下

--- auto_script/cron_pull_up.sh
+++ auto_script/cron_pull_up.sh
@@ -6,7 +6,7 @@ NUM0=`ps -ef | grep -i 'test_api.py' | grep -v "grep" | wc -l`  #查看程序进
 if [ $NUM0 -eq 0 ]; then
     echo "try start test_api.py.........."
     cd /Users/huxi/Desktop/crypto
-    nohup ./venv/bin/python3 ./test_api.py>./log/back_log_`date +%Y%m%d-%H:%M:%S`.txt  &              #后台启动程序，作为daemon
+    nohup ./venv/bin/python3 ./test_api.py>./log/running-log/back_log_`date +%Y%m%d-%H:%M:%S`.txt  &              #后台启动程序，作为daemon
     echo "test_api.py(crashed) restart date is : `date +%Y%m%d-%H:%M:%S`">>./log/test_api.py-crash-restart.log         #重启进程的重启日志
 else
     echo "test_api.py already running"
