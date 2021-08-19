9ff0ffda8a228cb9a46ac48771cbd3701a50ffa8
huxi <1324907549@qq.com>
2021-08-19 11:54:40 +0800

    [修改]1.on_line_updater.py 脚本本省没有循环，启动都是 crontab控制，log/on_line_updater.py-start.log 记录每次的启动时间          2.test_api 自己循环，on_line_updater.py-crash-start.log记录崩溃后，被crontab 重新拉起的记录

--- auto_script/cron_pull_up.sh
+++ auto_script/cron_pull_up.sh
@@ -7,7 +7,7 @@ if [ $NUM0 -eq 0 ]; then
     echo "try start test_api.py.........."
     cd /Users/huxi/Desktop/crypto
     nohup ./venv/bin/python3 ./test_api.py>./log/back_log_`date +%Y%m%d-%H:%M:%S`.txt  &              #后台启动程序，作为daemon
-    echo "test_api.py restart date is : `date +%Y%m%d-%H:%M:%S`">>./log/test_api.py-restart.log         #重启进程的重启日志
+    echo "test_api.py(crashed) restart date is : `date +%Y%m%d-%H:%M:%S`">>./log/test_api.py-crash-restart.log         #重启进程的重启日志
 else
     echo "test_api.py already running"
 fi
